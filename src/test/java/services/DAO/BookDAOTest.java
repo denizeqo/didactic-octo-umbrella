6cd7db12aa91d52a3ee37928a7978b2c94c21a38
Deni Zeqo <deni_zeqo@universitetipolis.edu.al>
2025-01-27 04:41:51 +0100

    changed ides eclipse was giving too many errors

---
+++ src/test/java/services/DAO/BookDAOTest.java
@@ -0,0 +1,12 @@
+package services.DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookDAOTest {
+
+    @Test
+    void checkAvailability() {
+    }
+}
